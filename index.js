6abd1640878647a1c54c9be9271b8e87c6fd05ee
ksama22 <ksama22@jaumebalmes.net>
2023-03-29 20:59:21 +0200

    part 2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function findPokemon() {
     callApi(input.toLowerCase())
 }
 
-function callApi(name) {
+function callApi(name, url = "https://pokeapi.co/api/v2/pokemon/") {
     var xhr = new XMLHttpRequest();
     xhr.addEventListener("readystatechange", function () {
         if (this.readyState === this.DONE) {
@@ -17,7 +17,7 @@ function callApi(name) {
             console.log(pokeIndex);
         }
     });
-    xhr.open("GET", "https://pokeapi.co/api/v2/pokemon/" + name);
+    xhr.open("GET", url + name);
     xhr.send(null);
 }
 
@@ -47,15 +47,18 @@ function pokeNext() {
 // 100 = x
 function refreshViewPokemon(poke) {
     document.getElementById("name").innerText = poke.name;
-    document.getElementById("types").innerText = getTypes(poke.types);
+    //document.getElementById("description").innerText = poke.species.url;
+    //document.getElementById("types").innerText = getTypes(poke.types);
     //Segons el tamany de la persona(167cm), el fica mes gran o mes petit
-    document.getElementById("image").src = poke.sprites.other["official-artwork"].front_default;
-    document.getElementById("image").height = poke.height * 10;
+    document.getElementById("image-shiny").src = poke.sprites.other["official-artwork"].front_shiny;
+    document.getElementById("image-normal").src = poke.sprites.other["official-artwork"].front_default;
+
+    //document.getElementById("image").height = poke.height * 10;
 }
 
 function refreshViewStadystics(poke) {
     //Height
-    document.getElementById("pokeHeight").innerText = poke.height * 10;
+    //document.getElementById("pokeHeight").innerText = poke.height * 10;
     //Stadystics
     document.getElementById("hp").innerText = poke.stats[0].base_stat; //0 = hp
     document.getElementById("attack").innerText = poke.stats[1].base_stat; //1 = attack
@@ -64,4 +67,10 @@ function refreshViewStadystics(poke) {
     document.getElementById("spDefense").innerText = poke.stats[4].base_stat; //4 = sp.defense
     document.getElementById("speed").innerText = poke.stats[5].base_stat;  //5 = speed
     document.getElementById("total").innerText = poke.stats[0].base_stat + poke.stats[1].base_stat + poke.stats[2].base_stat + poke.stats[3].base_stat + poke.stats[4].base_stat + poke.stats[5].base_stat;
+}
+
+function flipCard() {
+    let ncard = document.getElementById("flip-card-inner");
+    ncard.style.transform = ncard.style.transform ? "" : "rotateY(180deg)"
+    document.getElementById("flip-card-inner")
 }
